19a7896cbec29e60d8d2fcc174cf82f293f745e3
Washington Luis <luis.washington.ribeiro@gmail.com>
2025-02-17 08:58:32 -0300

    fix linting

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,5 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
-
 export default function Dashboard() {
-	const { onOpen } = useNewAccount()
-
 	return <div />
 }
