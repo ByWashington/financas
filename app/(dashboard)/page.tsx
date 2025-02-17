e2a9073d80b4049eeff7ee01645923bd6038b93c
Washington Luis <luis.washington.ribeiro@gmail.com>
2025-02-17 08:55:09 -0300

    Removido button da dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -6,9 +6,5 @@ import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
 export default function Dashboard() {
 	const { onOpen } = useNewAccount()
 
-	return (
-		<div>
-			<Button onClick={onOpen}>Adicionar uma conta</Button>
-		</div>
-	)
+	return <div />
 }
